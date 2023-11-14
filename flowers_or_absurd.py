66d8324861bc764b910a59bd96b9d2f542511f9f
hossein <Hossein.m.7782@gmail.com>
2023-11-14 12:13:57 +0330

    end

--- flowers_or_absurd.py
+++ flowers_or_absurd.py
@@ -1,3 +1,25 @@
 import os 
+import random 
 os.system('cls')
 
+input_cups =int(input('How many cups ? '))
+input_chances=int(input('How many chances ? '))
+
+def check_gessing():
+    ai_goal=random.randint(1,input_cups)
+    for i in range(input_chances):
+        print('-'*30)
+        print(f'{input_chances-i} chances left!')
+        user_choices=int(input('Enter you Gess: '))
+        if user_choices==ai_goal:
+            print('-'*30)
+            print('You won!')
+            return
+        else:
+            print('you gess is wrong.')
+            print('Please try again.')
+
+    print('-'*30)         
+    print('Sorry.You Lost!')
+
+check_gessing()
